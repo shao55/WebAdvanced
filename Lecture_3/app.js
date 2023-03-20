53ac94611ff5f31bfe754d84d82872a42c5cd86c
Nurgissa Itkarinov <nurgissa.22@gmail.com>
2023-03-20 11:23:28 +0600

    Lecture 3

---
+++ Lecture_3/app.js
@@ -0,0 +1,7 @@
+const _ = require("lodash"); // Библиотека для работы с массивами
+
+const oldArr = [1, [2, 3, [4, 5]]];
+
+const newArr = _.flattenDeep(oldArr); // Метод библиотеки
+
+console.log(newArr);
